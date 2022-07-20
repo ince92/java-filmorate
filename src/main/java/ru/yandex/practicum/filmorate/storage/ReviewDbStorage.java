e0e2ca68fd6486278f5d7d782b6e7d8f56ca3603
SergeevAleksandr <aa.sergeev.eltreit@yandex.ru>
2022-07-20 17:37:33 +0400

    Добавил функционал Отзывы

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewDbStorage.java
@@ -0,0 +1,142 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.storage.storageInterface.ReviewStorage;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Primary
+@Slf4j
+public class ReviewDbStorage implements ReviewStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReviewDbStorage(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Review create(Review review) {
+        String sqlQuery = "insert into REVIEWS (CONTENT, IS_POSITIVE, USER_ID,FILM_ID,USEFUL) values (?, ?, ?, ?,?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sqlQuery, new String[]{"REVIEW_ID"});
+            stmt.setString(1, review.getContent());
+            stmt.setBoolean(2, review.getIsPositive());
+            stmt.setLong(3, review.getUserId());
+            stmt.setLong(4, review.getFilmId());
+            stmt.setLong(5, review.getUseful());
+            return stmt;
+        }, keyHolder);
+        review.setReviewId(keyHolder.getKey().longValue());
+        log.info("Отзыв успешно создан, id - {}", review.getReviewId());
+        return review;
+    }
+
+    @Override
+    public Review update(Review review) {
+        String sqlQuery = "update REVIEWS set " +
+                "CONTENT = ?, IS_POSITIVE = ?, USER_ID = ?, FILM_ID = ?, USEFUL = ? " +
+                "where REVIEW_ID = ?";
+        jdbcTemplate.update(sqlQuery
+                , review.getContent()
+                , review.getIsPositive()
+                , review.getUserId()
+                , review.getFilmId()
+                , review.getUseful()
+                ,review.getReviewId());
+        log.info("Отзыв успешно обновлен, id - {}", review.getReviewId());
+        return review;
+    }
+    @Override
+    public Long deleteReview(Long reviewId){
+        String deleteReview = "DELETE FROM REVIEWS WHERE REVIEW_ID = ?";
+        jdbcTemplate.update(deleteReview, reviewId);
+        log.info("Отзыв успешно удален, id - {}", reviewId);
+        return reviewId;
+    }
+
+    @Override
+    public List<Review> findAll(int count) {
+        String sql = "select * from REVIEWS order by USEFUL desc limit ?";
+        return  jdbcTemplate.query(sql, (rs, rowNum) -> makeReview(rs),count);
+    }
+
+    public Optional<Review> findReviewById(long id)  {
+        String sql = "select * from REVIEWS where REVIEW_ID = ?";
+
+        List<Review> films = jdbcTemplate.query(sql, (rs, rowNum) -> makeReview(rs),id);
+        if (films.size()==0){
+            return Optional.empty();
+        }else{
+            return Optional.of(films.get(0));
+        }
+    }
+    public List<Review> findReviewByFilmId(long id,int count)  {
+        String sql = "select * from REVIEWS where FILM_ID = ?  order by USEFUL desc limit ?";
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeReview(rs),id,count);
+    }
+    @Override
+    public void addLike(long reviewId, long userId) {
+        String addLike = "insert into REVIEWS_LIKE (REVIEW_ID, USER_ID, LIKES) values (?, ?, ?)";
+        String updateUsefulPlus = "update REVIEWS set USEFUL = REVIEWS.USEFUL + 1 where REVIEW_ID = ?";
+        jdbcTemplate.update(addLike, reviewId, userId, true);
+        jdbcTemplate.update(updateUsefulPlus, reviewId);
+        log.info("Лайк добавлен");
+    }
+
+    @Override
+    public void addDislike(long reviewId, long userId) {
+        String addLike = "insert into REVIEWS_LIKE (REVIEW_ID, USER_ID, LIKES) values (?, ?, ?)";
+        String updateUsefulMinus = "update REVIEWS set USEFUL = USEFUL - 1 where REVIEW_ID = ?";
+        jdbcTemplate.update(addLike, reviewId, userId, false);
+        jdbcTemplate.update(updateUsefulMinus, reviewId);
+        log.info("Дизлайк добавлен");
+    }
+
+    @Override
+    public void deleteLike(long reviewId, long userId) {
+        String deleteLike = "delete from REVIEWS_LIKE where REVIEW_ID = ? and USER_ID = ?";
+        String updateUsefulMinus = "update REVIEWS set USEFUL = USEFUL - 1 where REVIEW_ID = ?";
+        if (jdbcTemplate.update(deleteLike, reviewId, userId) < 1) {
+            log.info("Лайк не найден");
+        } else {
+            jdbcTemplate.update(updateUsefulMinus, reviewId);
+            log.info("Лайк удалеен");
+        }
+
+    }
+
+    @Override
+    public void deleteDislike(long reviewId, long userId) {
+        String deleteLike = "delete from REVIEWS_LIKE where REVIEW_ID = ? and USER_ID = ?";
+        String updateUsefulPlus = "update REVIEWS set USEFUL = REVIEWS.USEFUL + 1 where REVIEW_ID = ?";
+        if (jdbcTemplate.update(deleteLike, reviewId, userId) < 1) {
+            log.info("Дизлайк не найден");
+        } else {
+            jdbcTemplate.update(updateUsefulPlus, reviewId);
+            log.info("Дизлайк удален");
+        }
+    }
+    public Review makeReview(ResultSet rs) throws SQLException {
+        long id = rs.getLong("REVIEW_ID");
+        String content = rs.getString("CONTENT");
+        boolean isPositive = rs.getBoolean("IS_POSITIVE");
+        long userId = rs.getLong("USER_ID");
+        long filmId = rs.getLong("FILM_ID");
+        int useful = rs.getInt("USEFUL");
+        return new Review(id,content,isPositive,userId,filmId,useful);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/storageInterface/ReviewStorage.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.storage.storageInterface;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Review;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewStorage {
+    Review create(Review review);
+    Review update(Review review);
+
+    Long deleteReview(Long reviewId);
+
+    List<Review> findAll(int count);
+    Optional<Review> findReviewById(long id);
+
+    List<Review> findReviewByFilmId(long filmId,int count);
+
+    void addLike(long reviewId, long userId);
+
+    void addDislike(long reviewId, long userId);
+
+    void deleteLike(long reviewId, long userId);
+
+    void deleteDislike(long reviewId, long userId);
+
+}
